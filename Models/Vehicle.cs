a8aaff12cc53c84751bcfc029b8eb17c00e9b608
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-01 19:13:07 -0500

    Adding first model

---
+++ Models/Vehicle.cs
@@ -0,0 +1,11 @@
+//Make sure to include the Vehicle class separate from the Program class
+public class Vehicle
+{
+    //Accessibility of class members is defaulted to private
+    //so we must add the public keyword to anything we
+    //want to allow outside access to.
+    
+    // this unassigned integer will default to 0
+    public int NumPassengers;
+}
+
