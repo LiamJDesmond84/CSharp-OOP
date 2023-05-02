2b4cd258c35494ff6ea0f628baaae22d8eddfa0b
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-01 19:15:45 -0500

    Adding first model

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,6 +1,5 @@
 //Make sure to include the Vehicle class separate from the Program class
-public class Vehicle
-{
+public class Vehicle {
     //Accessibility of class members is defaulted to private
     //so we must add the public keyword to anything we
     //want to allow outside access to.
