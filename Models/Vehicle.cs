f4bb2f64da6d02070875beed1f2fe341aadca619
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-02 20:13:41 -0500

    OOP Stuff

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,10 +1,10 @@
-//Make sure to include the Vehicle class separate from the Program class
+//* Make sure to include the Vehicle class separate from the Program class
 public class Vehicle {
-    //Accessibility of class members is defaulted to private
-    //so we must add the public keyword to anything we
-    //want to allow outside access to.
+    //* Accessibility of class members is defaulted to private
+    //* so we must add the public keyword to anything we
+    //* want to allow outside access to.
     
-    // this unassigned integer will default to 0
+    //* this unassigned integer will default to 0
     public int NumPassengers;
 }
 
