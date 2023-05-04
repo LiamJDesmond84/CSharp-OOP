141264f81b7e00a13ceaf2749851f5b7c6ccd77a
Liam Desmond <liamjdesmond84@gmail.com>
2023-05-03 19:17:26 -0500

    Messing with class members

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -35,5 +35,28 @@ public class Vehicle {
         {
             Console.WriteLine("BEEP!");
         }
+
+
+
+        public string ColorProp
+    {
+        get
+        {
+    	    //* Simply referencing the property will invoke the "getter", such as:
+    	    //* Console.WriteLine(vehicleObject.ColorProp);
+    	    //* and will return the following:
+    
+            return $"This thing is {Color}";
+        }
+        set
+        {
+    	    //* Assigning a value to a property, such as:
+    	    //* vehicleObject.ColorProp = "Blue";
+    	    //* will invoke the "setter", and the "value" keyword will become the assigned value
+    	    //* ("Blue" in this example)
+    
+            Color = value;
+        }
+    }
 }
 
